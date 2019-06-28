8583e45b9a8ea4a1d44fb40593f3db9b0caacf50
Vasilii <goooneoooogle@gmail.com>
2019-06-28 17:09:29 +0300

    Star DB: first settings

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,7 +1,28 @@
-import React, {Components} from 'react';
+import React from 'react';
+
+import Header from '../header';
+import RandomPlanet from '../random-planet';
+import ItemList from '../item-list';
+import PersonDetails from '../person-details';
+
 import './app.css';
 
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <RandomPlanet />
 
-export default class App extends Components {
+      <div className="row mb2">
+        <div className="col-md-6">
+          <ItemList />
+        </div>
+        <div className="col-md-6">
+          <PersonDetails />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-}
+export default App;
